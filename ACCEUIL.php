7014c885a6bbb92ce33d629dbacf5e2f0f0279b8
lina <lina12345.2002@gmail.com>
2021-03-03 23:09:31 +0100

    salam

---
+++ ACCEUIL.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="acceuil.css">
+    <title>acceuil</title>
+</head>
+<body>
+    <header>
+<ul>
+<li><a href="">FEMME</a></li>
+<li><a href="">HOMME</a></li>
+<li><a href="">Nourriture</a></li>
+<li><a href="">Materiels</a></li>
+</ul>
+<a href="">logo</a>
+
+
+    </header>
+</body>
+</html>
