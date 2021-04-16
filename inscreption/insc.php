676c6876c25005ae178e63b338dbc3f978ecf06b
joessir <lassiryyass@gmail.com>
2021-04-16 20:17:35 +0000

    .

---
+++ inscreption/insc.php
@@ -0,0 +1,33 @@
+<?php
+$nom=$_POST['nom'];
+$prenom=$_POST['prenom'];
+$login=$_POST['login'];
+$password=$_POST['password'];
+?>
+
+<?php
+
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=lyk;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+$userReq = $bdd -> prepare('INSERT INTO user(nom_user, prenom ,login,password)
+                            VALUES (:nom_user , :prenom ,:login,:password)');
+$userReq -> execute(array(
+    'nom_user' => $nom,
+    'prenom' => $prenom,
+    'login' => $login,
+    'password' => $password
+
+)) ;
+
+?>                       
+                          
+
+
+
